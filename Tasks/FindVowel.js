e32086c4fbc4afb1d26cb9462d6fee01e25ef2c4
sanykos <sanykos446@gmail.com>
2020-04-28 20:07:04 +0300

    Initial commit

---
+++ Tasks/FindVowel.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded',function(){
+    let str = 'Hello';
+
+    function findVowels(str) {
+        let count = 0;
+        const vowels = ['a', 'e', 'i', 'o'];
+        for(let char of str.toLowerCase()) {
+            if(vowels.includes(char)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    //console.log(findVowels(str));
+});
