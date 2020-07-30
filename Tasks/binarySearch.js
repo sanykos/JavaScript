a4fdcab2414431299011667f2b9645f8fa234496
sanykos <kosih2009@yandex.ru>
2020-07-30 15:27:11 +0300

    binarysearch

--- Tasks/binarySearch.js
+++ Tasks/binarySearch.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('binary')
+    
+    function binarySearch(arr, target) {
+        let start = 0;
+        let end = arr.length - 1;
+
+        while(start <= end) {
+            let middle = Math.floor((start + end) / 2)
+            if(target === arr[middle]) {
+                console.log(target)
+            }
+
+            if(target > arr[middle]) {
+                start = middle + 1
+            }
+
+            if(target < arr[middle]) {
+                end = middle - 1
+            }
+        }
+    }
+    
 })
