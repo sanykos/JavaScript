f63547b346dc2b3b1d8dce3cae1fb950f05716b0
Alexander <alexander.kosykh.fc@gmail.com>
2020-09-20 13:29:05 +0300

    Add sum function

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -1,15 +1,23 @@
-document.addEventListener('DOMContentLoaded' ,function(){
 /** sum(1)(2) */
 
-    function sum(a, b) {
+/* function sum(a, b) {
         if(b) {
             return a + b;
         }
         return function(c) {
             return a + c;
         }
-    }
+    }*/
 
-    console.log(sum(1)(2))
-
-})
+// console.log(sum(1)(2))
+/** sum(1)(2)(3)... */
+function sum(a) {
+  let result = a;
+  function f(b) {
+    result += b;
+    console.log(result);
+    return f;
+  }
+  return f;
+}
+sum(1)(2)(3);
